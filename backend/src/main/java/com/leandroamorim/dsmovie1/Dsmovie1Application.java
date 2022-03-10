4ad23fc7de1542dc4c13ae4f2e773b54569d4de6
Leandro Amorim <leandroamorimpb@gmail.com>
2022-03-10 00:37:17 -0300

    Projeto criado

---
+++ backend/src/main/java/com/leandroamorim/dsmovie1/Dsmovie1Application.java
@@ -0,0 +1,13 @@
+package com.leandroamorim.dsmovie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmovie1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmovie1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leandroamorim/dsmovie1/Dsmovie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroamorim.dsmovie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmovie1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
